29e075f84406197e6c9635d140079468e0597ec8
MCrow <35507241+RestoreMonarchy@users.noreply.github.com>
2021-04-17 22:24:24 +0200

    package dependency resolver

---
+++ src/tests/Shearlegs.Framework.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shearlegs.Framework.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
