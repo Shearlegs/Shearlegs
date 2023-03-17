ce6d5899002c6eadefd91392bde14e01c3db5fb4
MCrow <michalwrona@hotmail.com>
2023-03-17 12:18:36 +0100

    Add Blazor server client application

---
+++ src/web/Shearlegs.Web.Dashboard/Constants/ShearlegsTheme.cs
@@ -0,0 +1,31 @@
+﻿using MudBlazor;
+
+namespace Shearlegs.Web.Dashboard.Constants
+{
+    public class ShearlegsTheme
+    {
+        public static readonly MudTheme MudTheme = new()
+        {
+            Palette = new Palette()
+            {
+                Primary = Colors.Green.Darken3,
+                Secondary = Colors.Blue.Default,
+                AppbarBackground = Colors.Green.Darken3                
+            },
+            PaletteDark = new()
+            {
+                Primary = Colors.Green.Darken1,
+                Secondary = Colors.Blue.Default,
+                AppbarBackground = Colors.Green.Darken3,
+                Background = Colors.Grey.Darken4,
+                TextPrimary = Colors.Shades.White,
+                DrawerBackground = Colors.Grey.Darken3,
+                DrawerText = Colors.Shades.White,
+                DrawerIcon = Colors.Shades.White,
+                BackgroundGrey = Colors.Grey.Darken3,
+                TableLines = Colors.Grey.Darken2,
+                Surface = Colors.Grey.Darken3
+            }
+        };
+    }
+}
