2b91d41d4d8a42e6791cc2002ca8f5c68288d6e5
MCrow <michalwrona@hotmail.com>
2023-04-29 10:44:02 +0200

    Add node management page

---
+++ src/web/Shearlegs.Web.Dashboard/Models/Forms/Managements/Nodes/UpdateNodeFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shearlegs.Web.Dashboard.Models.Forms.Managements.Nodes
+{
+    public class UpdateNodeFormModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+        [StringLength(2000)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string FQDN { get; set; }
+        [Required]
+        public string Scheme { get; set; }
+        public bool IsBehindProxy { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ src/web/Shearlegs.Web.Dashboard/Pages/Managements/Nodes/NodeManagementPage.razor.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Shearlegs.Web.APIClient.Models.Exceptions;
+using Shearlegs.Web.APIClient.Models.Nodes;
+using Shearlegs.Web.APIClient.Models.Nodes.Requests;
+using Shearlegs.Web.APIClient.Models.Plugins.Requests;
+using Shearlegs.Web.Dashboard.Models.Forms.Managements.Nodes;
+using Shearlegs.Web.Dashboard.Models.Forms.Managements.Plugins;
+using System.Net;
+
+namespace Shearlegs.Web.Dashboard.Pages.Managements.Nodes
+{
+    public partial class NodeManagementPage
+    {
+        [Parameter]
+        public string NodeName { get; set; }
+
+        public List<BreadcrumbItem> BreadcrumbItems = new()
+        {
+            new BreadcrumbItem("Home", "/"),
+            new BreadcrumbItem("Management", "/management"),
+            new BreadcrumbItem("Nodes", "/management/nodes")
+        };
+
+        public Node Node { get; set; }
+        public UpdateNodeFormModel Model { get; set; }
+
+        private bool isLoaded = false;
+        private bool isCanceled = false;
+        private bool isUpdateUserProcessing = false;
+        private bool showSuccessAlert = false;
+        private bool showErrorAlert = false;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (BreadcrumbItems.Count > 3)
+            {
+                BreadcrumbItems.RemoveAt(3);
+            }
+
+            try
+            {
+                Node = await client.Nodes.GetNodeByNameAsync(NodeName);
+            }
+            catch (ShearlegsWebAPIRequestException exception) when (exception.StatusCode == HttpStatusCode.NotFound)
+            {
+                isCanceled = true;
+                BreadcrumbItems.Add(new BreadcrumbItem("Not Found", null, true));
+                return;
+            }
+
+            BreadcrumbItems.Add(new BreadcrumbItem(Node.Name, null, true));
+
+            Model = new()
+            {
+                Name = Node.Name,
+                Description = Node.Description,
+                FQDN = Node.FQDN,
+                Scheme = Node.Scheme,
+                IsBehindProxy = Node.IsBehindProxy,
+                IsEnabled = Node.IsEnabled
+            };
+
+            isLoaded = true;
+        }
+
+        private async Task HandleUpdateNodeAsync()
+        {
+            isUpdateUserProcessing = true;
+            showSuccessAlert = false;
+            showErrorAlert = false;
+
+            UpdateNodeRequest request = new()
+            {
+                Name = Model.Name,
+                Description = Model.Description,
+                FQDN = Model.FQDN,
+                Scheme = Model.Scheme,
+                IsBehindProxy = Model.IsBehindProxy,
+                IsEnabled = Model.IsEnabled
+            };
+
+            try
+            {
+                Node = await client.Nodes.UpdateNodeAsync(Node.Id, request);
+
+                if (Node.Name != NodeName)
+                {
+                    navigationManager.NavigateTo($"/management/nodes/{Node.Name}");
+                }
+
+                showSuccessAlert = true;
+            }
+            catch (ShearlegsWebAPIRequestException)
+            {
+                showErrorAlert = true;
+            }
+
+            isUpdateUserProcessing = false;
+        }
+    }
+}
